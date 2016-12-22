85ecc214a277f9cc0339ee3bff51a301ec503ccc
Gilberto Medeiros <gilberto.medeiros@miniclip.com>
2016-12-22 17:35:39 +0000

    improve card selection visualisation

--- public/cardVisual.js
+++ public/cardVisual.js
@@ -1,11 +1,16 @@
+handVisuals = [];
+
 class CardVisual {
   constructor(cardDef, handIndex) {
     this.cardDef = cardDef;
     this.handIndex = handIndex;
+
   }
 
   createVisual() {
     this.bg = cc.LayerColor.create(cc.color(0,200,0,255), 120, 200);
+    this.bg.anchorX = 0.5;
+    this.bg.anchorY = 0;
     var label = cc.LabelTTF.create(this.cardDef.name, "Arial", 16);
     label.anchorX = 0;
     label.x = 5;
@@ -13,31 +18,34 @@ class CardVisual {
     this.bg.addChild(label);
     gameScene.addChild(this.bg);
 
+    var menu = cc.Menu.create();
+    var size = cc.director.getWinSize();
+    menu.x -= size.width/2;
+    menu.y -= size.height/2;
+    this.bg.addChild(menu, 3);
     this.menuItem = cc.MenuItem.create(
                 function(val){
-                    console.log('clicked ' + this.cardDef.name);
                     socket.emit('play card', {'playerid':playerid, 'handIndex':this.handIndex});
+                    HighlightCard(this.handIndex);
                 },
               this);
     menu.addChild(this.menuItem);
     this.menuItem.width = this.bg.width;
     this.menuItem.height = this.bg.height;
-    this.menuItem.x = this.bg.x;
-    this.menuItem.y = this.bg.y;
     this.menuItem.anchorX = 0;
     this.menuItem.anchorY = 0;
 
     return this.bg;
   }
 
-  setPos(x, y) {
-    this.bg.x = x;
-    this.bg.y = y;
-    this.menuItem.x = this.bg.x;
-    this.menuItem.y = this.bg.y;
+  setHighlight(highlight) {
+    if (highlight) {
+      this.bg.scale = 1.1;
+    }
+    else {
+      this.bg.scale = 1.0;
+    }
   }
-
-
 }
 
 function CreateHand(hand) {
@@ -46,9 +54,20 @@ function CreateHand(hand) {
     console.log('hand[' + handIndex + '] = ' + hand[handIndex]);
     var vis = new CardVisual(getCardDefById(hand[handIndex]), handIndex);
     var root = vis.createVisual();
-    vis.setPos(80 + offset, 50)
-    //root.x = 80 + offset;
-    //root.y = 50;
+    root.x = 80 + offset;
+    root.y = 50;
     offset += 150;
+    handVisuals.push(vis);
+  }
+}
+
+function HighlightCard (visualIndex) {
+  for (var index in handVisuals) {
+    var visual = handVisuals[index];
+    if (index == visualIndex) {
+      visual.setHighlight(true);
+    } else {
+      visual.setHighlight(false);
+    }
   }
 }
