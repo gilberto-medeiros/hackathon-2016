11ce5d497c1f120eb5b6257a9a671b4c20853d5a
Ricardo Vladimiro <ricardo.vladimiro@miniclip.com>
2016-12-22 14:24:45 +0000

    Cleaned up playCard and addCardToHand

--- shared/deck.js
+++ shared/deck.js
@@ -1,27 +1,20 @@
 class Deck {
 
   constructor(deckDefinition) {
-    //console.log('Creating Deck');
     this.definition = deckDefinition;
     this.hand = new Array();
     this.addCardToHand();
-
-
   }
 
   playCard(cardIndex) {
-    var card = this.deck.hand[cardIndex];
-    this.deck.hand.splice(cardIndex, 1);
-    addCardToHand();
-    return card;
+    var cardID = this.hand.splice(cardIndex, 1);
+    this.addCardToHand();
+    return cardID;
   }
 
   addCardToHand() {
-    console.log('Adding cards to hand.');
-    console.log(this.hand);
-    while (this.hand.length == undefined || this.hand.lenght < 5) {
-      var cardID = this.definition.getRandomCardDefinition().id;
-      console.log('Added ' + cardID);
+    while (this.hand.length == undefined || this.hand.length < 5) {
+      var cardID = this.definition.cardsDefinition.getRandomCardDefinition().id;
       this.hand.push(cardID);
     }
   }
